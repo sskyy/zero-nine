305efa4db14c3815cb5b15b2347f0d86ab32cc98
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-11-03 20:13:28 +0800

    add empty group

--- public/modules/index/index.js
+++ public/modules/index/index.js
@@ -5,6 +5,9 @@ angular.module('index',['ui.router','countdown','tomato','user.session','group']
   .controller( 'index',function($scope,session,$http){
     $scope.user = session.data('user')
     $scope.$watch( 'user.id',function(id){
+      if( !$scope.user.groups){
+        $scope.user.groups = []
+      }
       if( id && $scope.user.groups.length !== 0 && !_.isObject($scope.user.groups[0] )){
         $http.get("/user/"+$scope.user.id + "?populate=groups").success(function(user){
           $scope.user = user
